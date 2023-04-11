41979c535b03454bb4733c42d601af98bd3b3946
JasonSnow123 <m.jason.2018@gmail.com>
2023-04-11 18:53:24 -0400

    Changes auth provider to nextauth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import { UserProvider } from "@auth0/nextjs-auth0/client";
 
 export default function App() {
-  return (
-    <UserProvider>
-      <h1>Hello</h1>
-    </UserProvider>
-  );
+  return <h1>Hello</h1>;
 }
