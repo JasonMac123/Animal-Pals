6b87b3ca93fd576ff68e28bc3cb694cfbb076756
JasonSnow123 <m.jason.2018@gmail.com>
2023-04-11 19:14:51 -0400

    Fixes tailwindcss not working properly

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from "react";
+import "../styles/globals.css";
 
 export default function App() {
-  return <div>Animal Pals</div>
+  return <div className="text-rose-500 text-2xl">Animal Pals</div>;
 }
