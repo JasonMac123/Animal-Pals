afe56fbc6b44005d85a4d88f1474690c8da98689
JasonSnow123 <m.jason.2018@gmail.com>
2023-04-14 16:21:04 -0400

    Adds registerModal and handling form submission

---
+++ src/components/modals/RegisterModal.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import axios from "axios";
+import { AiFillFacebook } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { useState, useCallback } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+import useRegister from "../hooks/useRegister";
+import Modal from "./Modal";
+
+const RegisterModal = () => {
+  const registerModal = useRegister();
+  const [loading, setLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setLoading(true);
+
+    axios
+      .post("/api/register", data)
+      .then(() => {
+        registerModal.onClose();
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return (
+    <Modal
+      disabled={loading}
+      isOpen={registerModal.isOpen}
+      title="Register"
+      actionLabel="Continue"
+      onClose={registerModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+    />
+  );
+};
+
+export default RegisterModal;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,13 @@ import "../styles/globals.css";
 import NavBar from "@/components/navbar/NavBar";
 import Client from "@/components/Client";
 import Modal from "@/components/modals/Modal";
+import RegisterModal from "@/components/modals/RegisterModal";
 
 export default function App() {
   return (
     <>
       <Client>
-        <Modal title="hello-world" isOpen actionLabel="Submit" />
+        <RegisterModal />
         <NavBar />
       </Client>
     </>
