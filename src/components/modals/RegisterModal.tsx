40e32df814731a4d3154e22b32e01cd69af2ed7a
JasonSnow123 <m.jason.2018@gmail.com>
2023-04-16 13:58:55 -0400

    Create input for each form

--- src/components/modals/RegisterModal.tsx
+++ src/components/modals/RegisterModal.tsx
@@ -9,6 +9,7 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import useRegister from "../hooks/useRegister";
 import Modal from "./Modal";
 import Heading from "../Heading";
+import Input from "../inputs/Input";
 
 const RegisterModal = () => {
   const registerModal = useRegister();
@@ -45,6 +46,7 @@ const RegisterModal = () => {
   const bodyContent = (
     <div className="flex flex-col gap-4">
       <Heading title="Welcome to Animal-Pals" subtitle="Create an Account" />
+      <Input />
     </div>
   );
 
