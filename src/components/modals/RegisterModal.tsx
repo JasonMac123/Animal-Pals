dc2a4367589016d6f7e991904a29be99d4e81ee7
JasonSnow123 <m.jason.2018@gmail.com>
2023-04-16 13:56:58 -0400

    Adds heading component for titles

--- src/components/modals/RegisterModal.tsx
+++ src/components/modals/RegisterModal.tsx
@@ -8,6 +8,7 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 import useRegister from "../hooks/useRegister";
 import Modal from "./Modal";
+import Heading from "../Heading";
 
 const RegisterModal = () => {
   const registerModal = useRegister();
@@ -41,6 +42,12 @@ const RegisterModal = () => {
       });
   };
 
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading title="Welcome to Animal-Pals" subtitle="Create an Account" />
+    </div>
+  );
+
   return (
     <Modal
       disabled={loading}
@@ -49,6 +56,7 @@ const RegisterModal = () => {
       actionLabel="Continue"
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
     />
   );
 };
