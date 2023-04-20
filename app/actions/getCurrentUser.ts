0e31ad91efb9f8f078c8ffa9ccadb4cc70f520b5
JasonSnow123 <m.jason.2018@gmail.com>
2023-04-20 15:17:39 -0400

    Revert "changes status code on register and fixes session bug error"
    This reverts commit 7224cf8b75fa0281e717fa266e75bfdad0074481.

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -26,7 +26,10 @@ export default async function getCurrentUser() {
     }
 
     return {
-      currentUser,
+      ...currentUser,
+      createdAt: currentUser.createdAt.toISOString(),
+      updatedAt: currentUser.updatedAt.toISOString(),
+      emailVerified: currentUser.emailVerified?.toISOString() || null,
     };
   } catch (error: any) {
     return null;
