76a4e800ff8d6876058eb53fb324c39ef8085f7b
JasonSnow123 <m.jason.2018@gmail.com>
2023-04-20 15:16:07 -0400

    Revert "Changes spacing for containers"
    This reverts commit 59f3d62a7af2989cad9e945fc8d4e7bbf324b862.

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -9,7 +9,7 @@ const Heading: React.FC<HeadingProps> = ({ title, subtitle }) => {
   return (
     <div className="text-center">
       <div className="text-2xl font-bold">{title}</div>
-      <div className="font-light mt-3">{subtitle}</div>
+      <div className="font-light mt-2">{subtitle}</div>
     </div>
   );
 };
