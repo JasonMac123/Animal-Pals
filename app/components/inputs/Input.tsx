838edfbd2aeb7f0248d0bcbc6232686f9ebf751b
JasonSnow123 <m.jason.2018@gmail.com>
2023-04-20 15:15:55 -0400

    Revert "Changes colour scheme of error on input component"
    This reverts commit 3b704d7cdc09f782c604bd6430ac9169b798f2ab.

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -35,13 +35,13 @@ const Input: React.FC<InputProps> = ({
         type={type}
         className={`peer w-full p-4 pt-6 font-light bg-white border-2 rounded-md outline-none transition disabled:opacity-70 disabled:cursor-not-allowed 
         ${formatPrice ? "pl-99" : "pl-4"}
-        ${errors[id] ? "border-rose-600" : "border-slate-300"} 
-        ${errors[id] ? "focus:border-rose-600" : "focus:border-black"}`}
+        ${errors[id] ? "border-rose-500" : "border-slate-300"} 
+        ${errors[id] ? "focus:border-rose-500" : "focus:border-black"}`}
       />
       <label
         className={`absolute text-sm duration-150 transform -translate-y-3 top-5 z-10 origin-[0] 
         ${formatPrice ? "left-9" : "left-4"} 
-        ${errors[id] ? "text-rose-600" : ""}
+        ${errors[id] ? "text-rose-500" : ""}
         peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4`}
       >
         {label}
