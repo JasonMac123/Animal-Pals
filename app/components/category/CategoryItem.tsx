1de185cae3a4f127a79b7ade803082c7a0b9dd0c
JasonSnow123 <m.jason.2018@gmail.com>
2023-04-21 18:12:55 -0400

    Adds categories to filter resources

--- app/components/category/CategoryItem.tsx
+++ app/components/category/CategoryItem.tsx
@@ -1,4 +1,8 @@
 "use client";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+import queryString from "query-string";
+import { useCallback } from "react";
 import { IconType } from "react-icons";
 
 interface CategoryItemProps {
@@ -13,8 +17,35 @@ const CategoryItem: React.FC<CategoryItemProps> = ({
   label,
   selected,
 }) => {
+  const router = useRouter();
+  const params = useSearchParams();
+
+  const handleClick = useCallback(() => {
+    let currentQuery = {};
+
+    if (params) {
+      currentQuery = queryString.parse(params.toString());
+    }
+
+    const updatedQuery: any = {
+      category: label,
+    };
+
+    if (params?.get("category") === label) {
+      updatedQuery.category = null;
+    }
+
+    const url = queryString.stringifyUrl(
+      { url: "/", query: updatedQuery },
+      { skipNull: true }
+    );
+
+    router.push(url);
+  }, [router, label, params]);
+
   return (
     <div
+      onClick={handleClick}
       className={`flex flex-col items-center gap-2 hover:text-neutral-700 transition cursor-pointer 
       ${selected ? "bg-neutral-600 text-white" : ""} `}
     >
