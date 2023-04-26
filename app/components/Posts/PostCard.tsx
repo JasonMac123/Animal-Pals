b9eb650e8b89802625995c16423f8e0143240e03
JasonSnow123 <m.jason.2018@gmail.com>
2023-04-26 15:39:38 -0400

    Add props to allow user to make reservation

--- app/components/Posts/PostCard.tsx
+++ app/components/Posts/PostCard.tsx
@@ -1,7 +1,13 @@
-import { Post } from "@prisma/client";
+import { Post, Reservation } from "@prisma/client";
+import { safeUser } from "../../types/user";
 
 interface PostCardProps {
   post: Post;
+  reservation?: Reservation;
+  onAction?: (id: string) => void;
+  actionLabel?: string;
+  actionId?: string;
+  currentUser?: safeUser | null;
 }
 
 const PostCard: React.FC<PostCardProps> = ({ post }) => {
