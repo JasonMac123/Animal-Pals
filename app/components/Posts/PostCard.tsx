7ef250f681731e8a3ec8044b558e6e89bda14efe
JasonSnow123 <m.jason.2018@gmail.com>
2023-04-26 15:52:59 -0400

    Adds package to help format dates from prisma

--- app/components/Posts/PostCard.tsx
+++ app/components/Posts/PostCard.tsx
@@ -1,5 +1,11 @@
+"use client";
+
 import { Post, Reservation } from "@prisma/client";
 import { safeUser } from "../../types/user";
+import { useRouter } from "next/navigation";
+import { useCallback, useMemo } from "react";
+
+import { format } from "date-fns";
 
 interface PostCardProps {
   post: Post;
@@ -10,7 +16,42 @@ interface PostCardProps {
   currentUser?: safeUser | null;
 }
 
-const PostCard: React.FC<PostCardProps> = ({ post }) => {
+const PostCard: React.FC<PostCardProps> = ({
+  post,
+  reservation,
+  onAction,
+  actionLabel,
+  actionId = "",
+  currentUser,
+}) => {
+  const router = useRouter();
+
+  const handleCancel = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.stopPropagation();
+      onAction?.(actionId);
+    },
+    [onAction, actionId]
+  );
+
+  const price = useMemo(() => {
+    if (reservation) {
+      return reservation.totalPrice;
+    }
+
+    return post.price;
+  }, [reservation, post.price]);
+
+  const reservationDate = useMemo(() => {
+    if (!reservation) {
+      return null;
+    }
+    const start = new Date(reservation.startDate);
+    const end = new Date(reservation.endDate);
+
+    return `${format(start, "PP")} - ${format(end, "PP")}`;
+  }, [reservation]);
+
   return <div>ListingCard</div>;
 };
 
