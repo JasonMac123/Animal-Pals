abb48ed6d14a3f8fa7b58b6469cd52c3a78d0184
JasonSnow123 <m.jason.2018@gmail.com>
2023-05-01 15:06:08 -0400

    Fixes color scheme of navbar

--- app/components/category/CategoryItem.tsx
+++ app/components/category/CategoryItem.tsx
@@ -46,7 +46,7 @@ const CategoryItem: React.FC<CategoryItemProps> = ({
   return (
     <div
       onClick={handleClick}
-      className={`flex flex-col items-center gap-2 hover:text-neutral-700 transition cursor-pointer w-9/12 py-4 rounded-xl bg-
+      className={`flex flex-col items-center gap-2 hover:bg-neutral-700 hover:text-white transition cursor-pointer w-9/12 py-4 rounded-xl bg-
       ${selected ? "bg-black text-white" : ""} `}
     >
       <Icon size={26} />
