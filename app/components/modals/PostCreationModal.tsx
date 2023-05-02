624e850b89a165379792fe22cadf1cb45b666e8d
JasonSnow123 <m.jason.2018@gmail.com>
2023-05-02 15:18:11 -0400

    Adds input for heading

--- app/components/modals/PostCreationModal.tsx
+++ app/components/modals/PostCreationModal.tsx
@@ -1,10 +1,42 @@
 "use client";
 
-import useCreatePost from "../hooks/useCreatePost";
+import Heading from "../Heading";
 import Modal from "./Modal";
+import Input from "../inputs/Input";
+import useCreatePost from "../hooks/useCreatePost";
+import { useForm, FieldValues } from "react-hook-form";
 
 const PostCreationModal = () => {
   const postCreationModal = useCreatePost();
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm<FieldValues>({
+    defaultValues: {
+      dogs: true,
+      cats: true,
+      maxOccupancy: 1,
+      imageSrc: "",
+      price: 1,
+      title: "",
+      description: "",
+    },
+  });
+
+  const bodyContent = (
+    <div className="flex flex-col gap-8">
+      <Heading
+        title="Describe your animal vacation home!"
+        subtitle="Get short and straight to the point!"
+      />
+      <Input id="title" label="Title" register={register} errors={errors} />
+    </div>
+  );
 
   return (
     <Modal
