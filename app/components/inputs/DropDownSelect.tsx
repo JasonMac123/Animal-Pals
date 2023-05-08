c3b760e3b27be88f3b4e047d70a1f151e195afe2
JasonSnow123 <m.jason.2018@gmail.com>
2023-05-08 15:02:27 -0400

    Adds react-select into drop down select

--- app/components/inputs/DropDownSelect.tsx
+++ app/components/inputs/DropDownSelect.tsx
@@ -1,7 +1,20 @@
-const DropDownSelect = () => {
-  return (
-    <div>DropDownSelect</div>
-  )
-}
+"use client";
 
-export default DropDownSelect
+import Select from "react-select";
+
+interface DropDownSelectProps {}
+
+const DropDownSelect: React.FC<DropDownSelectProps> = () => {
+  return (
+    <div>
+      <Select
+        placeholder="Select Toronto region below"
+        isClearable
+        value={value}
+        onChange={(value) => onChange(value)}
+      />
+    </div>
+  );
+};
+
+export default DropDownSelect;
