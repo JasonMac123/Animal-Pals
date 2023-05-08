afa7b2adfc6be4537210b9d89f4e4b4fa37cde40
JasonSnow123 <m.jason.2018@gmail.com>
2023-05-08 15:19:03 -0400

    Adds object typing to drop down regions

--- app/components/inputs/DropDownSelect.tsx
+++ app/components/inputs/DropDownSelect.tsx
@@ -2,16 +2,38 @@
 
 import Select from "react-select";
 
-interface DropDownSelectProps {}
+const regions = [
+  { value: "Brampton", label: "Brampton" },
+  { value: "Etobicoke", label: "Etobicoke" },
+  { value: "Markham", label: "Markham" },
+  { value: "Midtown", label: "Midtown" },
+  { value: "Missisuaga", label: "Missisuaga" },
+  { value: "Newmarket", label: "Newmarket" },
+  { value: "North York", label: "North York" },
+  { value: "Richmond Hill", label: "Richmond Hill" },
+  { value: "Scarborough", label: "Scarborough" },
+  { value: "Vaughan", label: "Vaughan" },
+];
 
-const DropDownSelect: React.FC<DropDownSelectProps> = () => {
+export type RegionValue = {
+  label: string;
+  value: string;
+};
+
+interface DropDownSelectProps {
+  value?: RegionValue;
+  onChange: (value: RegionValue) => void;
+}
+
+const DropDownSelect: React.FC<DropDownSelectProps> = ({ value, onChange }) => {
   return (
     <div>
       <Select
         placeholder="Select Toronto region below"
         isClearable
+        options={regions}
         value={value}
-        onChange={(value) => onChange(value)}
+        onChange={(value) => onChange(value as RegionValue)}
       />
     </div>
   );
