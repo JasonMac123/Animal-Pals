7d3a2ba55215ddb186adb8b8f5aa4e0c6711bfe7
JasonSnow123 <m.jason.2018@gmail.com>
2023-05-19 15:22:41 -0400

    Separates post page into different sections

--- app/post/[postId]/Post.tsx
+++ app/post/[postId]/Post.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import { Reservation } from "@prisma/client";
 import { safePost, safeUser } from "../../types/types";
+import PostFront from "./PostFront";
 
 interface PostProps {
   reservations?: Reservation[];
@@ -10,7 +13,19 @@ interface PostProps {
 }
 
 const Post: React.FC<PostProps> = ({ post, currentUser }) => {
-  return <div>Post</div>;
+  return (
+    <div className="mx-auto">
+      <div className="flex flex-col gap-4">
+        <PostFront
+          title={post.title}
+          imageSrc={post.imageSrc}
+          region={post.region}
+          id={post.id}
+          currentUser={currentUser}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Post;
---
+++ app/post/[postId]/PostFront.tsx
@@ -0,0 +1,5 @@
+const PostFront = () => {
+  return <div></div>;
+};
+
+export default PostFront;
