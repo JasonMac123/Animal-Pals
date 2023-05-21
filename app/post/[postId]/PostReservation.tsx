1bda7880b7b68af5a2f4a67fe398b703cfee5663
JasonSnow123 <m.jason.2018@gmail.com>
2023-05-21 16:30:38 -0400

    Adds react-date-fns to create calendar

--- app/post/[postId]/PostReservation.tsx
+++ app/post/[postId]/PostReservation.tsx
@@ -1,7 +1,38 @@
 "use client";
 
-const PostReservation = () => {
-  return <div></div>;
+import { Calendar, Range } from "react-date-range";
+
+interface PostReservationProps {
+  price: number;
+  totalPrice: number;
+  dateRange: Range;
+  onChangeDate: (value: Range) => void;
+  onSubmit: () => void;
+  bookedDates: Date[];
+}
+
+const PostReservation: React.FC<PostReservationProps> = ({
+  price,
+  dateRange,
+  totalPrice,
+  onChangeDate,
+  onSubmit,
+  bookedDates,
+}) => {
+  return (
+    <div className="rounded-xl border-[1px] border-neutral-400 overflow-hidden">
+      <div className="flex items-center gap-1 p-4">
+        <div className="text-2xl">$ {price}</div>
+        <div>/ night</div>
+      </div>
+      <hr />
+      <Calendar
+        value={dateRange}
+        bookedDates={bookedDates}
+        onChange={(value) => onChangeDate(value.selection)}
+      />
+    </div>
+  );
 };
 
 export default PostReservation;
