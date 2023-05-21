297a7406cdbdbba2c6d765f791fb4536d50e073c
JasonSnow123 <m.jason.2018@gmail.com>
2023-05-21 16:39:54 -0400

    Uses react-date range Calendar to choose dates

--- app/components/inputs/Calendar.tsx
+++ app/components/inputs/Calendar.tsx
@@ -1,7 +1,32 @@
 "use client";
 
-const Calendar = () => {
-  return <div></div>;
+import { DateRange, Range, RangeKeyDict } from "react-date-range";
+
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+
+interface CalendarProps {
+  value: Range;
+  onChange: (value: RangeKeyDict) => void;
+  bookedDates: Date[];
+}
+
+const Calendar: React.FC<CalendarProps> = ({
+  value,
+  onChange,
+  bookedDates,
+}) => {
+  return (
+    <DateRange
+      rangeColors={["ff002b"]}
+      ranges={[value]}
+      date={new Date()}
+      onChange={onChange}
+      showDateDisplay={false}
+      minDate={new Date()}
+      disabledDates={bookedDates}
+    />
+  );
 };
 
 export default Calendar;
