ac747c84166a9c206fba39227570acaeebf54c32
JasonSnow123 <m.jason.2018@gmail.com>
2023-05-23 14:58:12 -0400

    Fixes type error

--- app/types/types.ts
+++ app/types/types.ts
@@ -4,9 +4,13 @@ export type safePost = Omit<Post, "createdAt"> & {
   createdAt: string;
 };
 
-export type safeUser = Omit<User, "createdAt" | "updatedAt"> & {
+export type safeUser = Omit<
+  User,
+  "createdAt" | "updatedAt" | "emailVerified"
+> & {
   createdAt: string;
   updatedAt: string;
+  emailVerified: string | null;
 };
 
 export type typeSafeReservations = Omit<
