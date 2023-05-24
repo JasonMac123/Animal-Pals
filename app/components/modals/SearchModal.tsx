b8b671647a4cd05b20fad1a8603f9c3ab3bb6c34
JasonSnow123 <m.jason.2018@gmail.com>
2023-05-24 16:43:31 -0400

    Adds dropdown menu to select region for filtering

--- app/components/modals/SearchModal.tsx
+++ app/components/modals/SearchModal.tsx
@@ -1,8 +1,55 @@
+"use client";
+import qs from "query-string";
 import useSearch from "../hooks/useSearch";
+import { useRouter, useSearchParams } from "next/navigation";
 import Modal from "./Modal";
+import { useCallback, useState } from "react";
+import Heading from "../Heading";
+import DropDownSelect from "../inputs/DropDownSelect";
 
 const SearchModal = () => {
   const searchModal = useSearch();
+  const params = useSearchParams();
+  const router = useRouter();
+
+  const [petCount, setPetCount] = useState(1);
+  const [region, setRegion] = useState({
+    value: "Brampton",
+    label: "Brampton",
+  });
+
+  const onSubmit = useCallback(async () => {
+    let query = {};
+
+    if (params) {
+      query = qs.parse(params.toString());
+    }
+
+    const newQuery: any = {
+      ...query,
+      region: region.value,
+      maxOccupancy: petCount,
+    };
+
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: newQuery,
+      },
+      { skipNull: true }
+    );
+
+    searchModal.onClose();
+
+    router.push(url);
+  }, [searchModal, location, router, petCount, region, params]);
+
+  let bodyContent = (
+    <div className="flex flex-col gap-8">
+      <Heading title="What area are you looking for?" />
+      <DropDownSelect value={region} onChange={(value) => setRegion(value)} />
+    </div>
+  );
 
   return (
     <Modal
