c3383c50ce92403e42c784ba9958e90fcc498bdc
JasonSnow123 <m.jason.2018@gmail.com>
2023-05-25 15:01:38 -0400

    Adds filtering for posts when searching

--- app/functions/getPosts.ts
+++ app/functions/getPosts.ts
@@ -1,8 +1,40 @@
 import prisma from "../prisma/prismadb";
 
-export default async function getPosts() {
+export interface postParams {
+  userId?: string;
+  maxOccupancy?: number;
+  region: string;
+  category: string;
+}
+
+export default async function getPosts(params: postParams) {
   try {
+    const { userId, maxOccupancy, region, category } = params;
+
+    let query: any = {};
+
+    if (userId) {
+      query.userId = userId;
+    }
+
+    if (maxOccupancy) {
+      query.maxOccupancy = {
+        gte: +maxOccupancy,
+      };
+    }
+
+    if (category) {
+      query.animals = {
+        hasEvery: category,
+      };
+    }
+
+    if (region) {
+      query.region = region;
+    }
+
     const posts = await prisma.post.findMany({
+      where: query,
       orderBy: {
         createdAt: "desc",
       },
