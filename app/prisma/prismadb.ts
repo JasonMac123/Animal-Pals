9ef2ae528a66db27c893a48e832fe3bd21aef94c
JasonSnow123 <m.jason.2018@gmail.com>
2023-06-04 12:17:27 -0400

    Fixes prisma not being defined

--- app/prisma/prismadb.ts
+++ app/prisma/prismadb.ts
@@ -4,13 +4,7 @@ declare global {
   var prisma: PrismaClient;
 }
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+const prisma = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
 
 export default prisma;
